8d3a4c2199839b1e7d96866ae90a4d96f95285d3
zhen639 <zhen6939@gmail.com>
2016-07-20 19:32:33 +0800

    output fuzz when input 3

--- app/fb_generator.rb
+++ app/fb_generator.rb
@@ -1,5 +1,8 @@
 class FbGenerator
   def self.output(param)
+    if param == 3
+      return 'Fuzz'
+    end
     param.to_s
   end
 end
--- spec/fb_generator_spec.rb
+++ spec/fb_generator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe FbGenerator, type: :model do
     it 'return 1 when input 1' do
       expect(FbGenerator.output(1)).to eq('1')
     end
+
+    it 'return Fuzz when input 3' do
+      expect(FbGenerator.output(3)).to eq('Fuzz')
+    end
   end
 end
