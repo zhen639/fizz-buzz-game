ee4a00e145c07a9f20216cdbf5339e0e586d1922
zhen639 <zhen6939@gmail.com>
2016-07-20 20:12:59 +0800

    return FuzzBuzz when number is a common multiple of 3 and 5

--- app/fb_generator.rb
+++ app/fb_generator.rb
@@ -1,10 +1,12 @@
 class FbGenerator
   def self.output(param)
     (1..param).map do |num|
-      if num % 3 == 0
-        next 'Fuzz'
+      if num % 15 == 0
+        next 'FuzzBuzz'
       elsif num % 5 == 0
         next 'Buzz'
+      elsif num % 3 == 0
+        next 'Fuzz'
       end
       num.to_s
     end
--- spec/fb_generator_spec.rb
+++ spec/fb_generator_spec.rb
@@ -3,23 +3,32 @@ require_relative '../app/fb_generator'
 RSpec.describe FbGenerator, type: :model do
   describe '#ouput' do
     it 'return 1 when input 1' do
-      expect(FbGenerator.output(1)).to match_array(['1'])
+      expect(FbGenerator.output(1)).to eq(['1'])
     end
 
     it "return [1, 2, Fuzz] when input 3" do
-      expect(FbGenerator.output(3)).to match_array(['1', '2', 'Fuzz'])
+      expect(FbGenerator.output(3)).to eq(['1', '2', 'Fuzz'])
     end
 
     it "return [1, 2, Fuzz, 4, Buzz] when input 5" do
-      expect(FbGenerator.output(5)).to match_array(['1', '2', 'Fuzz', '4', 'Buzz'])
+      expect(FbGenerator.output(5)).to eq(['1', '2', 'Fuzz', '4', 'Buzz'])
     end
 
     it 'return Fuzz when number is a multiple of 3' do
-      expect(FbGenerator.output(6)).to match_array(['1', '2', 'Fuzz', '4', 'Buzz', 'Fuzz'])
+      expect(FbGenerator.output(6)).to eq(['1', '2', 'Fuzz', '4', 'Buzz', 'Fuzz'])
     end
 
     it 'return Buzz when number is a multiple of 5' do
-      expect(FbGenerator.output(10)).to match_array(['1', '2', 'Fuzz', '4', 'Buzz', 'Fuzz', '7', '8', 'Fuzz', 'Buzz'])
+      expect(FbGenerator.output(10)).to eq(['1', '2', 'Fuzz', '4', 'Buzz', 'Fuzz', '7', '8', 'Fuzz', 'Buzz'])
+    end
+
+    it 'return FuzzBuzz when number is a common multiple of 3 and 5' do
+      expect(FbGenerator.output(15)).to eq(
+        [
+          '1', '2', 'Fuzz', '4', 'Buzz',
+          'Fuzz', '7', '8', 'Fuzz', 'Buzz',
+          '11', 'Fuzz', '13', '14', 'FuzzBuzz'
+        ])
     end
   end
 end
